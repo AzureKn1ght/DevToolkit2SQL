61a0210e7cfad402825c81a12b0797bdaaf17ad1
Philson Nah <philsonnah@msn.com>
2021-06-28 11:22:56 +0800

    added sql file

---
+++ queries.sql
@@ -0,0 +1,14 @@
+use market; --use a particular db
+
+show tables; --show the tables in the db
+
+describe products; --describe a particular table
+
+sEleCt * FrOm pRoDucTs; --MySQL is not case sensitive
+
+select name from products; --show only particular column
+
+--select multiple columns
+select name,market_price from products; 
+
+select * from customers;
