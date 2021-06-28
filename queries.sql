efd3bdce324de8710258c81cd458a548ad77c5ac
Philson Nah <philsonnah@msn.com>
2021-06-28 11:43:55 +0800

    updated format

--- queries.sql
+++ queries.sql
@@ -1,14 +1,45 @@
-use market; --use a particular db
-
-show tables; --show the tables in the db
-
-describe products; --describe a particular table
-
-sEleCt * FrOm pRoDucTs; --MySQL is not case sensitive
-
-select name from products; --show only particular column
-
+--use a particular db
+use market;
+--
+--show the tables in the db
+show tables;
+--
+--describe a particular table
+describe products;
+--
+--MySQL is not case sensitive
+sEleCt *
+FrOm pRoDucTs;
+--
+--show only particular column
+select name
+from products;
+--
 --select multiple columns
-select name,market_price from products; 
-
-select * from customers;
+select name,
+    market_price
+from products;
+select *
+from customers;
+--
+--verbose specification
+select products.name,
+    products.market_price
+from products;
+--
+--giving the result alias
+select name as "Product Name",
+    market_price as "Market Price"
+from products;
+--
+--alis in the FROM to shorten the select
+--but you must use "p" now, you cannot use "products."
+select p.name,
+    p.market_price
+from products as p;
+--
+--Adding conditions to select statement
+select *
+from customers
+where wallet > 7500
+    and tolerance > 0.5;
