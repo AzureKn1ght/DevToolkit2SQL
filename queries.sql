ed75d9f82164564c6b44c7705d2d098170f0affe
Philson Nah <philsonnah@msn.com>
2021-06-28 12:11:41 +0800

    added the "in" command

--- queries.sql
+++ queries.sql
@@ -1,45 +1,125 @@
 --use a particular db
 use market;
+
 --
 --show the tables in the db
 show tables;
+
 --
 --describe a particular table
 describe products;
+
 --
 --MySQL is not case sensitive
-sEleCt *
-FrOm pRoDucTs;
+sEleCt
+    *
+FrOm
+    pRoDucTs;
+
 --
 --show only particular column
-select name
-from products;
+select
+    name
+from
+    products;
+
 --
 --select multiple columns
-select name,
+select
+    name,
     market_price
-from products;
-select *
-from customers;
+from
+    products;
+
+select
+    *
+from
+    customers;
+
 --
 --verbose specification
-select products.name,
+select
+    products.name,
     products.market_price
-from products;
+from
+    products;
+
 --
 --giving the result alias
-select name as "Product Name",
+select
+    name as "Product Name",
     market_price as "Market Price"
-from products;
+from
+    products;
+
 --
 --alis in the FROM to shorten the select
 --but you must use "p" now, you cannot use "products."
-select p.name,
+select
+    p.name,
     p.market_price
-from products as p;
+from
+    products as p;
+
 --
 --Adding conditions to select statement
-select *
-from customers
-where wallet > 7500
+select
+    *
+from
+    customers
+where
+    wallet > 7500
     and tolerance > 0.5;
+
+--
+--Sort the results
+select
+    *
+from
+    products
+order by
+    market_price ASC;
+
+--
+--Multiple level sorting conditions
+--(The order of presidence matters)
+select
+    *
+from
+    products
+order by
+    market_price DESC,
+    name ASC;
+
+--
+--Limit the number of results
+select
+    *
+from
+    products
+where
+    market_price > 10
+order by
+    market_price DESC
+limit
+    5;
+
+--
+--How do we shorten this?
+select
+    *
+from
+    transactions
+where
+    product_id = 2
+    or product_id = 4
+    or product_id = 6;
+
+--
+--Using the "in" keyword, not the best performance tho
+select
+    *
+from
+    transactions
+where
+    product_id in (2, 4, 6)
